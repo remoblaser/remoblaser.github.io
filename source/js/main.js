904bd654fea8dc42a5845cf03f389a1d81846166
Remo Blaser <remo.blaser@gmail.com>
2015-12-28 21:32:45 +0100

    Fixed search JS error

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,14 @@
-SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: 'http://blog.remoblaser.ch/search.json',
-  searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
-  noResultsText: '<li class="no-results">No results found :(</li>',
-  limit: 10,
-  fuzzy: false
-});
+if(document.getElementById('search-input') != null) {
+  SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: 'http://blog.remoblaser.ch/search.json',
+    searchResultTemplate: '<li><a href="{url}" title="{title}">{title}</a></li>',
+    noResultsText: '<li class="no-results">No results found :(</li>',
+    limit: 10,
+    fuzzy: false
+  });
+}
 
 $(document).ready(function() {
   $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
